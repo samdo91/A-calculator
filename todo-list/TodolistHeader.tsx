b7ae756bd03ff847d9da4dc3162986c74edcd4a6
samdo91 <102779069+samdo91@users.noreply.github.com>
2022-09-22 19:11:44 +0900

    Delete TodolistHeader.tsx

--- todo-list/TodolistHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TodoListHeader = () =>{
-
-    return(<>   <h1 className="headerTitle"><mark className="mark5">5</mark>가지 할일!</h1> 
-          </>)
-}   
-
-export default TodoListHeader
